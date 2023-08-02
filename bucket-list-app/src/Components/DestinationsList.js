91381463ba442ed83182255b811e6795d8ab044a
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-08-02 10:40:04 +0300

    update on destinationlist.js

--- bucket-list-app/src/Components/DestinationsList.js
+++ bucket-list-app/src/Components/DestinationsList.js
@@ -9,28 +9,46 @@ const DestinationsList = () => {
       .then((data) => setDestinations(data));
   }, []);
 
+  const [showDetails, setShowDetails] = useState({});
+
+  const handleImageClick = (destinationId) => {
+    setShowDetails((prevShowDetails) => ({
+      ...prevShowDetails,
+      [destinationId]: !prevShowDetails[destinationId]
+    }));
+  };
+
   return (
     <div>
       <h1>Travel Bucket List</h1>
       <ul>
         {destinations.map((destination) => (
           <li key={destination.id}>
-            <img src={destination.image} alt={destination.name} />
+            <img
+              src={destination.image}
+              alt={destination.name}
+              onClick={() => handleImageClick(destination.id)}
+            />
             <h2>{destination.name}</h2>
             <p>{destination.description}</p>
-            <h3>Popular Attractions:</h3>
-            <ul>
-              {destination.popularAttractions &&
-                destination.popularAttractions.map((attraction, index) => (
-                  <li key={index}>{attraction}</li>
-                ))}
-            </ul>
-            <p>
-              <strong>Best Time to Visit:</strong> {destination.bestTimeToVisit}
-            </p>
-            <p>
-              <strong>Safety Tips:</strong> {destination.safetyTips}
-            </p>
+            {showDetails[destination.id] && (
+              <div>
+                <h3>Popular Attractions:</h3>
+                <ul>
+                  {destination.popularAttractions &&
+                    destination.popularAttractions.map((attraction, index) => (
+                      <li key={index}>{attraction}</li>
+                    ))}
+                </ul>
+                <p>
+                  <strong>Best Time to Visit:</strong>{" "}
+                  {destination.bestTimeToVisit}
+                </p>
+                <p>
+                  <strong>Safety Tips:</strong> {destination.safetyTips}
+                </p>
+              </div>
+            )}
             <button>Add to Bucket List</button>
           </li>
         ))}
