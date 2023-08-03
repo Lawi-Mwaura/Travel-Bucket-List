79848659be4bdb4272c595d504cd3cbcca6f82c6
Mercy <mercy.mwongeli@student.moringaschool.com>
2023-08-03 16:14:02 +0300

    updated the bucketlist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
- // App.js
- import React from "react";
- import Search from "./Components/Search";
- import AddDestination from "./Components/AddDestination";
- import DestinationsList from "./Components/DestinationsList";
- import DestinationDetails from "./Components/DestinationDetails";
- import BucketList from "./Components/BucketList";
- 
+import React from "react";
+import Search from "./Components/Search";
+import AddDestination from "./Components/AddDestination";
+import DestinationsList from "./Components/DestinationsList";
+import DestinationDetails from "./Components/DestinationDetails";
+import BucketList from "./Components/BucketList";
+import "./App.css"; // Import the styles.css file
 
- function App() {
-   return (
-     <div>
-       <Search />
-       <AddDestination />
-       <DestinationsList />
-       <DestinationDetails />
-       
-     </div>
-   );
- }
- 
- export default App;
- 
+function App() {
+  return (
+    <div className="container">
+      <div className="main-content">
+        <Search />
+        <AddDestination />
+        <DestinationsList />
+        <DestinationDetails />
+      </div>
+      <div className="sidebar">
+        <BucketList />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/Components/BucketList.js
+++ src/Components/BucketList.js
@@ -2,15 +2,37 @@ import React, { useState, useEffect } from "react";
 
 const BucketList = () => {
   const [bucketList, setBucketList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:5000/bucketList")
-      .then((response) => response.json())
-      .then((data) => setBucketList(data));
+    fetch("http://localhost:5000/destinations")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setBucketList(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching bucket list:", error);
+        setLoading(false);
+      });
   }, []);
 
+  const handleViewDetails = (destinationId) => {
+    // Implement your logic to view the details of the selected destination
+    console.log("View details of destination with ID:", destinationId);
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>
+    <div className="sidebar">
       <h2>My Travel Bucket List</h2>
       {bucketList.length === 0 ? (
         <p>Your bucket list is empty. Start adding destinations!</p>
@@ -18,13 +40,12 @@ const BucketList = () => {
         <ul>
           {bucketList.map((destination) => (
             <li key={destination.id}>
-              <img
-                src={destination.image}
-                alt={destination.name}
-              />
+              <img src={destination.image} alt={destination.name} />
               <h3>{destination.location}</h3>
               <p>{destination.description}</p>
-              <button>View Details</button>
+              <button onClick={() => handleViewDetails(destination.id)}>
+                add bucketList
+              </button>
             </li>
           ))}
         </ul>
