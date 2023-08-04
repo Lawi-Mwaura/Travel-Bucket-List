53c076aa844f3bd0349627b1869c8afbdaed660e
Nasra Kassim <ninyuchinasra@gmail.com>
2023-08-04 11:11:39 +0300

    updates

--- bucket-list-app/src/Components/Search.js
+++ bucket-list-app/src/Components/Search.js
@@ -1,40 +1,55 @@
-import React, { useState } from "react";
-
-const Search = ({ destinations }) => {
-  const [suggestions, setSuggestions] = useState([]);
-
-  const getSuggestions = (value) => {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-
-    if (inputLength === 0) {
-      setSuggestions([]); // Clear suggestions when input is empty
-    } else if (destinations) { // Check if destinations is defined
-      const filteredSuggestions = destinations.filter((destination) => {
-        return (
-          destination.name && // Check if destination.name is defined
-          destination.name.toLowerCase().slice(0, inputLength) === inputValue
-        );
-      });
-
-      setSuggestions(filteredSuggestions);
-    }
-  };
-
-  return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search destinations..."
-        onChange={(e) => getSuggestions(e.target.value)}
-      />
-      <ul>
-        {suggestions.map((suggestion) => (
-          <li key={suggestion.id}>{suggestion.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Search;
+ // export default Search;
+ import React, { useState } from "react";
+ import Autosuggest from "react-autosuggest";
+ import { Link } from "react-router-dom";
+ 
+ const Search = ({ destinations }) => {
+   const [value, setValue] = useState("");
+   const [suggestions, setSuggestions] = useState([]);
+ 
+   const getSuggestions = (inputValue) => {
+     const inputValueLower = inputValue.toLowerCase();
+     return destinations.filter((destination) => destination.name && destination.name.toLowerCase().includes(inputValueLower));
+   };
+ 
+   const renderSuggestion = (suggestion) => (
+     <Link to={`/destination/${suggestion.id}`}>
+       {suggestion.name}
+     </Link>
+   );
+ 
+   const onSuggestionsFetchRequested = ({ value }) => {
+     setSuggestions(getSuggestions(value));
+   };
+ 
+   const onSuggestionsClearRequested = () => {
+     setSuggestions([]);
+   };
+ 
+   const inputProps = {
+     placeholder: "Search for countries...",
+     type: "text",
+     value,
+     className: "form-control", // Add Bootstrap class for styling
+     onChange: (event, { newValue }) => {
+       setValue(newValue);
+       setSuggestions(getSuggestions(newValue));
+     }
+   };
+ 
+   return (
+     <div className="my-4">
+       <Autosuggest
+         suggestions={suggestions}
+         onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+         onSuggestionsClearRequested={onSuggestionsClearRequested}
+         getSuggestionValue={(suggestion) => suggestion.name}
+         renderSuggestion={renderSuggestion}
+         inputProps={inputProps}
+       />
+     </div>
+   );
+ };
+ 
+ export default Search;
+ 
