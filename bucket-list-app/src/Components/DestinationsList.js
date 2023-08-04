2b935390249c5da317d6a80087710437b7f8e5f5
Nasra Kassim <ninyuchinasra@gmail.com>
2023-08-04 09:35:02 +0300

    css

--- bucket-list-app/src/Components/DestinationsList.js
+++ bucket-list-app/src/Components/DestinationsList.js
@@ -1,34 +1,3 @@
-//  // DestinationsList.js
-// import React, { useState, useEffect } from "react";
-
-// const DestinationsList = ({ onSelectDestination }) => {
-//   const [destinations, setDestinations] = useState([]);
-
-//   useEffect(() => {
-//     fetch("http://localhost:5000/destinations")
-//       .then((response) => response.json())
-//       .then((data) => setDestinations(data));
-//   }, []);
-
-//   return (
-//     <div>
-//       <h2>Travel Destinations</h2>
-//       {destinations.map((destination) => (
-//         <div key={destination.id}>
-//           <img src={destination.image} alt={destination.name} />
-//           <h3>{destination.name}</h3>
-//           <p>{destination.description}</p>
-//           <button onClick={() => onSelectDestination(destination)}>
-//             View Details
-//           </button>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default DestinationsList;
- 
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
@@ -44,14 +13,33 @@ const DestinationsList = ({ onSelectDestination }) => {
   return (
     <div>
       <h2>Travel Destinations</h2>
-      {destinations.map((destination) => (
-        <div key={destination.id}>
-          <img src={destination.image} alt={destination.name} />
-          <h3>{destination.name}</h3>
-          <p>{destination.description}</p>
-          <Link to={`/destination/${destination.id}`}>View Details</Link>
-        </div>
-      ))}
+
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {destinations.map((destination) => (
+          <div
+            key={destination.id}
+            className="card"
+            style={{ width: "18rem", margin: "10px", position: "relative" }}
+          >
+            <img src={destination.image} className="card-img-top" alt={destination.name} />
+            <div className="card-body">
+              <h5 className="card-title">{destination.name}</h5>
+              <p className="card-text">{destination.description}</p>
+              <Link
+                to={`/destination/${destination.id}`}
+                className="btn btn-primary"
+                style={{
+                  position: "absolute",
+                  bottom: "10px",
+                  right: "10px",
+                }}
+              >
+                View Details
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- bucket-list-app/src/index.js
+++ bucket-list-app/src/index.js
@@ -17,6 +17,7 @@
 // reportWebVitals();
 import React from "react";
 import ReactDOM from "react-dom";
+import './index.css';
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
