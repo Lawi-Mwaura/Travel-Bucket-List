4bce14262fc3c00a0cc9c17b87e8b39208be37e5
Nasra Kassim <ninyuchinasra@gmail.com>
2023-08-04 09:39:59 +0300

    changes

--- bucket-list-app/src/Components/BucketList.js
+++ bucket-list-app/src/Components/BucketList.js
@@ -1,38 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { Link } from "react-router-dom";
-
-// const BucketList = () => {
-//   const [bucketList, setBucketList] = useState([]);
-
-//   useEffect(() => {
-//     // Fetch the user's bucket list from the json-server
-//     fetch("http://localhost:5000/bucketlist")
-//       .then((response) => response.json())
-//       .then((data) => setBucketList(data));
-//   }, []);
-
-//   return (
-//     <div>
-//       <h2>Bucket List</h2>
-//       {bucketList.length > 0 ? (
-//         <ul>
-//           {bucketList.map((destination) => (
-//             <li key={destination.id}>
-//               <Link to={`/destination/${destination.id}`}>
-//                 {destination.name}
-//               </Link>
-//             </li>
-//           ))}
-//         </ul>
-//       ) : (
-//         <p>Your bucket list is empty.</p>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default BucketList;
-
 import React from "react";
 import { Link } from "react-router-dom";
 
--- bucket-list-app/src/Components/DestinationDetails.js
+++ bucket-list-app/src/Components/DestinationDetails.js
@@ -1,26 +1,3 @@
-// import React from "react";
-
-// const DestinationDetails = ({ destination, onBackButtonClick }) => {
-//   return (
-//     <div>
-//       <h2>{destination.name}</h2>
-//       <img src={destination.image} alt={destination.name} />
-//       <p>{destination.description}</p>
-//       <h3>Popular Attractions</h3>
-//       <ul>
-//         {destination.popularAttractions.map((attraction) => (
-//           <li key={attraction}>{attraction}</li>
-//         ))}
-//       </ul>
-//       <p>Best Time to Visit: {destination.bestTimeToVisit}</p>
-//       <p>Safety Tips: {destination.safetyTips}</p>
-//       <button onClick={onBackButtonClick}>Back</button>
-//     </div>
-//   );
-// };
-
-// export default DestinationDetails;
-
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 
